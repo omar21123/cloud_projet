f7facce59a962cfcef230c7a3f8c6d959c7ac708
OMAR EL BOUHSAINI <elbouhsainiomar@gmail.com>
2026-04-16 12:53:35 +0100

    Update Footer.jsx

--- frontend/src/sections/Footer.jsx
+++ frontend/src/sections/Footer.jsx
@@ -188,7 +188,7 @@ const Footer = () => {
               target={"_blank"}
               rel="dofollow noreferrer"
             >
-              Omar bsbtrhdrth
+              Omar boogeyman
             </a>
           </span>
         </Bottom>
