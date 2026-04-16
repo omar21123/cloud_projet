333d40c951fa2feae99a090ae2e9aaeeee96d9e5
OMAR EL BOUHSAINI <elbouhsainiomar@gmail.com>
2026-04-16 12:32:28 +0100

    Update Footer.jsx

--- frontend/src/sections/Footer.jsx
+++ frontend/src/sections/Footer.jsx
@@ -188,7 +188,7 @@ const Footer = () => {
               target={"_blank"}
               rel="dofollow noreferrer"
             >
-              Omar hhhhhhhhhhhhhhhhhhhhhhhh
+              Omar hhhhhhhhhhhhhhhhhhhhhhhhooooooooooooooooo
             </a>
           </span>
         </Bottom>
